fd72f0b184490437fdd4c2482646a173b408e930
photonactivity <72370775+photonactivity@users.noreply.github.com>
2021-05-26 01:39:09 +0800

    s1

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,8 +5,4 @@ export class AppService {
   getHello(): string {
     return 'photondance api';
   }
-
-  getHelloName(name: string): string {
-    return `Hello ${name}!`;
-  }
 }
--- src/users/dto/update-user.input.ts
+++ src/users/dto/update-user.input.ts
@@ -1,7 +1,6 @@
 import { CreateUserInput } from './create-user.input';
 import { InputType, Field, PartialType } from '@nestjs/graphql';
 
-// TODO 
 @InputType()
 export class UpdateUserInput extends PartialType(CreateUserInput) {
    @Field(() => String)
